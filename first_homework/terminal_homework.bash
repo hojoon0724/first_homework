5b8bb2c46b4b2d58bfb0907e9d8d547a5d3035ab
Hojoon Kim <hojoon0724@gmail.com>
2023-11-02 14:36:25 -0700

    terminal HW done

--- first_homework/terminal_homework.bash
+++ first_homework/terminal_homework.bash
@@ -1,4 +1,4 @@
-Last login: Thu Nov  2 12:29:54 on ttys002
+Last login: Thu Nov  2 14:07:58 on ttys007
 
 ~ 
 ❯ ls
@@ -29,11 +29,16 @@ sealday1
 ❯ cd galaxy_far_far_away 
 
 ~/Desktop/galaxy_far_far_away 
-❯ touch darth_vader.txt princess_leia.txt storm_trooper.txt
+❯ mkdir death_star
 
 ~/Desktop/galaxy_far_far_away 
-❯ ls
-darth_vader.txt		princess_leia.txt	storm_trooper.txt
+❯ cd death_star 
+
+Desktop/galaxy_far_far_away/death_star 
+❯ touch darth_vader.txt princess_leia.txt storm_trooper.txt
+
+Desktop/galaxy_far_far_away/death_star 
+❯ cd ..
 
 ~/Desktop/galaxy_far_far_away 
 ❯ mkdir tatooine
@@ -45,13 +50,298 @@ Desktop/galaxy_far_far_away/tatooine
 ❯ touch luke.txt ben_kenobi.txt
 
 Desktop/galaxy_far_far_away/tatooine 
-❯ mkdir millenium_falcon
+❯ mkdir millennium_falcon
 
 Desktop/galaxy_far_far_away/tatooine 
-❯ cd millenium_falcon 
+❯ cd m
+cd: no such file or directory: m
 
-galaxy_far_far_away/tatooine/millenium_falcon 
+Desktop/galaxy_far_far_away/tatooine 
+❯ cd millennium_falcon 
+
+galaxy_far_far_away/tatooine/millennium_falcon 
 ❯ touch han_solo.txt chewbaca.txt
 
-galaxy_far_far_away/tatooine/millenium_falcon 
+galaxy_far_far_away/tatooine/millennium_falcon 
+❯ ls
+chewbaca.txt	han_solo.txt
+
+galaxy_far_far_away/tatooine/millennium_falcon 
+❯ cd ..
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ ls
+ben_kenobi.txt		luke.txt		millennium_falcon
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ ls
+death_star	tatooine
+
+~/Desktop/galaxy_far_far_away 
+❯ cd death_star 
+
+Desktop/galaxy_far_far_away/death_star 
+❯ ls
+darth_vader.txt		princess_leia.txt	storm_trooper.txt
+
+Desktop/galaxy_far_far_away/death_star 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ cd tatooine 
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ mv ben_kenobi.txt obi_wan.txt
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ ls
+luke.txt		millennium_falcon	obi_wan.txt
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ cd death_star 
+
+Desktop/galaxy_far_far_away/death_star 
+❯ cp storm_trooper.txt ../tatooine 
+
+Desktop/galaxy_far_far_away/death_star 
+❯ ls
+darth_vader.txt		princess_leia.txt	storm_trooper.txt
+
+Desktop/galaxy_far_far_away/death_star 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ cd tatooine 
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ ls
+luke.txt		obi_wan.txt
+millennium_falcon	storm_trooper.txt
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ mv luke.txt millennium_falcon 
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ ls
+millennium_falcon	obi_wan.txt		storm_trooper.txt
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ mv obi_wan.txt millennium_falcon 
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ mv millennium_falcon ..
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ ls
+death_star		millennium_falcon	tatooine
+
+~/Desktop/galaxy_far_far_away 
+❯ mv millennium_falcon death_star 
+
+~/Desktop/galaxy_far_far_away 
+❯ cd death_star 
+
+Desktop/galaxy_far_far_away/death_star 
+❯ ls
+darth_vader.txt		princess_leia.txt
+millennium_falcon	storm_trooper.txt
+
+Desktop/galaxy_far_far_away/death_star 
+❯ mv princess_leia.txt millennium_falcon 
+
+Desktop/galaxy_far_far_away/death_star 
+❯ cd millennium_falcon 
+
+galaxy_far_far_away/death_star/millennium_falcon 
+❯ ls
+chewbaca.txt		luke.txt		princess_leia.txt
+han_solo.txt		obi_wan.txt
+
+galaxy_far_far_away/death_star/millennium_falcon 
+❯ rm obi_wan.txt 
+
+galaxy_far_far_away/death_star/millennium_falcon 
+❯ cd ..
+
+Desktop/galaxy_far_far_away/death_star 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ mkdir yavin_4
+
+~/Desktop/galaxy_far_far_away 
+❯ cd death_star                   
+
+Desktop/galaxy_far_far_away/death_star 
+❯ mv millennium_falcon ../yavin_4 
+
+Desktop/galaxy_far_far_away/death_star 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ cd yavin_4 
+
+Desktop/galaxy_far_far_away/yavin_4 
+❯ mkdir x_wing
+
+Desktop/galaxy_far_far_away/yavin_4 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ ls
+death_star	tatooine	yavin_4
+
+~/Desktop/galaxy_far_far_away 
+❯ cd death_star 
+
+Desktop/galaxy_far_far_away/death_star 
+❯ ls
+darth_vader.txt		storm_trooper.txt
+
+Desktop/galaxy_far_far_away/death_star 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ cd tatooine 
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ ls
+storm_trooper.txt
+
+Desktop/galaxy_far_far_away/tatooine 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ cd yavin_4 
+
+Desktop/galaxy_far_far_away/yavin_4 
+❯ ls   
+millennium_falcon	x_wing
+
+Desktop/galaxy_far_far_away/yavin_4 
+❯ cd millennium_falcon 
+
+galaxy_far_far_away/yavin_4/millennium_falcon 
+❯ ls
+chewbaca.txt		luke.txt
+han_solo.txt		princess_leia.txt
+
+galaxy_far_far_away/yavin_4/millennium_falcon 
+❯ mv princess_leia.txt ../x_wing 
+
+galaxy_far_far_away/yavin_4/millennium_falcon 
+❯ mv luke.txt ../x_wing 
+
+galaxy_far_far_away/yavin_4/millennium_falcon 
+❯ cd ..
+
+Desktop/galaxy_far_far_away/yavin_4 
+❯ mv millennium_falcon x_wing ..
+
+Desktop/galaxy_far_far_away/yavin_4 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ ls
+death_star		tatooine		yavin_4
+millennium_falcon	x_wing
+
+~/Desktop/galaxy_far_far_away 
+❯ cd death_star 
+
+Desktop/galaxy_far_far_away/death_star 
+❯ mkdir tie_fighter_1 tie_fighter_2 tie_fighter_3
+
+Desktop/galaxy_far_far_away/death_star 
+❯ ls
+darth_vader.txt		tie_fighter_1		tie_fighter_3
+storm_trooper.txt	tie_fighter_2
+
+Desktop/galaxy_far_far_away/death_star 
+❯ mv darth_vader.txt tie_fighter_1
+
+Desktop/galaxy_far_far_away/death_star 
+❯ cp storm_trooper.txt tie_fighter_2          
+
+Desktop/galaxy_far_far_away/death_star 
+❯ cp storm_trooper.txt tie_fighter_3
+
+Desktop/galaxy_far_far_away/death_star 
+❯ mv tie_fighter_1 tie_fighter_2 tie_fighter_3 ..
+
+Desktop/galaxy_far_far_away/death_star 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ ls
+death_star		tie_fighter_1		x_wing
+millennium_falcon	tie_fighter_2		yavin_4
+tatooine		tie_fighter_3
+
+~/Desktop/galaxy_far_far_away 
+❯ rm tie_fighter_2 tie_fighter_3
+rm: tie_fighter_2: is a directory
+rm: tie_fighter_3: is a directory
+
+~/Desktop/galaxy_far_far_away 
+❯ rm -r tie_fighter_2 tie_fighter_3
+
+~/Desktop/galaxy_far_far_away 
+❯ ls
+death_star		tatooine		x_wing
+millennium_falcon	tie_fighter_1		yavin_4
+
+~/Desktop/galaxy_far_far_away 
+❯ cd x_wing 
+
+Desktop/galaxy_far_far_away/x_wing 
+❯ toucd the_force.txt
+zsh: command not found: toucd
+
+Desktop/galaxy_far_far_away/x_wing 
+❯ touch the_force.txt
+
+Desktop/galaxy_far_far_away/x_wing 
+❯ cd ..
+
+~/Desktop/galaxy_far_far_away 
+❯ ls
+death_star		tatooine		x_wing
+millennium_falcon	tie_fighter_1		yavin_4
+
+~/Desktop/galaxy_far_far_away 
+❯ rm -r death_star 
+
+~/Desktop/galaxy_far_far_away 
+❯ ls
+millennium_falcon	tie_fighter_1		yavin_4
+tatooine		x_wing
+
+~/Desktop/galaxy_far_far_away 
+❯ mv millennium_falcon x_wing yavin_4 
+
+~/Desktop/galaxy_far_far_away 
+❯ ls   
+tatooine	tie_fighter_1	yavin_4
+
+~/Desktop/galaxy_far_far_away 
+❯ cd yavin_4 
+
+Desktop/galaxy_far_far_away/yavin_4 
+❯ ls
+millennium_falcon	x_wing
+
+Desktop/galaxy_far_far_away/yavin_4 
+❯ touch ghosts_of_ded_jedi.txt prequels_are_coming_eww.txt
+
+Desktop/galaxy_far_far_away/yavin_4 
 ❯ 
